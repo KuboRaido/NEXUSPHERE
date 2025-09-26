55575006ef4b5d87ef18e3d3de9e1c0656fcd209
kotomi <SBA24J90009@edu.sba.ac.jp>
2025-09-26 23:43:11 +0900

    DM一覧と会話画面

---
+++ nexusphere/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>メッセージ一覧</title>
+  <link rel="stylesheet" href="{{ asset('css/dm.css') }}">
+</head>
+<body>
+  <div class="phone-frame">
+    <h1>メッセージ一覧</h1>
+
+    <div id="chat-box">
+      <!-- JavaScriptでスレッド一覧を挿入 -->
+    </div>
+  </div>
+
+  <script src="{{ asset('js/dm.js') }}"></script>
+</body>
+</html>
