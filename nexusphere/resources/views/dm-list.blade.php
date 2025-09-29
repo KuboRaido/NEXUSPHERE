e3964ccf8d6e560ba35c95b27bc0b34f283062ee
kotomi <SBA24J90009@edu.sba.ac.jp>
2025-09-29 19:17:19 +0900

    index関係のものをすべてdm-listに変更

--- nexusphere/resources/views/dm-list.blade.php
+++ nexusphere/resources/views/dm-list.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>メッセージ一覧</title>
-  <link rel="stylesheet" href="{{ asset('css/dm_index.css') }}">
+  <link rel="stylesheet" href="{{ asset('css\dm-list.css') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
@@ -23,6 +23,6 @@
 
 </div>  
 
-  <script src="{{ asset('js/dm-index.js') }}"></script>
+  <script src="{{ asset('js/dm-list.js') }}"></script>
 </body>
 </html>
