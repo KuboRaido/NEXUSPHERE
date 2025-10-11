1329585779cc471f5aa0322b411532fe5bed7085
kotomi <SBA24J90009@edu.sba.ac.jp>
2025-10-11 18:12:39 +0900

    table名の変更

--- nexusphere/app/Models/Pcr.php
+++ nexusphere/app/Models/Pcr.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pcr extends Model
 {
-    protected $table = 'pcrs';
+    protected $table = 'prcs';
     protected $fillable = ['user_id','sentence','type','parent_id','circle_id','content'];
     public function user()
     {
