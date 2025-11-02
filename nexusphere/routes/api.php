125b3bf6ff13da788bc2661849fe3c4abe0601d4
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2025-11-02 18:33:36 +0900

    スペルミスの修正

--- nexusphere/routes/api.php
+++ nexusphere/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DmController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CircleController;
-use App\Http\Controllers\PcrController;
+use App\Http\Controllers\PrcController;
 
 Route::prefix('v1')->group(function(){
     Route::get('dmlist',[DmController::class,'dmlistback'])->name('dm.list'); // ← 認証なし
@@ -17,5 +17,5 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
     Route::post('/profile/edit/update',[ProfileController::class,'update'])->name('profile-update');//プロフィール更新処理
     Route::get('users/search',[UserController::class,'search'])->name('users.search'); // ← ユーザー検索追加
     Route::get('circle',[CircleController::class,'circleback'])->name('circle.back');
-    Route::get('post',[PcrController::class,'postback'])->name('post.back');
+    Route::get('post',[PrcController::class,'postback'])->name('post.back');
 });
