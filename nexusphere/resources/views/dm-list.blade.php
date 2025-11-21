9d05930e655b5917297f2c17c857fbf6459a2d4d
kotomi <SBA24J90009@edu.sba.ac.jp>
2025-11-21 16:26:44 +0900

    画面修正

--- nexusphere/resources/views/dm-list.blade.php
+++ nexusphere/resources/views/dm-list.blade.php
@@ -11,23 +11,24 @@
 </head>
 <body>
     <div class="header">
-      <span class="title">DM</span>
+      <span class="title">nexusphere</span>
     </div>
-    
-    <div class="search-container">
-      <input type="text" id="search-input" placeholder="ユーザーを検索..." />
-    </div>
-    {{--検索結果表示--}}
-    <ul id="search-results" class="search-results" style="display: none;"></ul>
+    <div class=dmlist-container>
+          <div class="search-container">
+            <input type="text" id="search-input" placeholder="ユーザーを検索..." />
+          </div>
+          {{--検索結果表示--}}
+          <ul id="search-results" class="search-results" style="display: none;"></ul>
 
-     <ul id="dm-list" class="dm-list" data-chat-url-template="{{url('/dm')}}?to=__ID__"></ul>
-    
-    <div class="footer-nav">
-      <a href="/home" class="tab active" data-target="home"><i class="fa-solid fa-house"></i></a>
-      <a href="/post" class="tab active" data-target="post"><i class="fas fa-paper-plane"></i></a>
-      <a href="/dmlist" class="tab active" data-target="talk"><i class="fa-solid fa-comment"></i></a>
-      <a href="/profile" class="tab active" data-target="mypage"><i class="fa-solid fa-user"></i></a>
-      <a href="/circle" class="tab active" data-target="circle"><i class="fa-solid fa-cube"></i></i></a>
+          <ul id="dm-list" class="dm-list" data-chat-url-template="{{url('/dm')}}?to=__ID__"></ul>
+          
+          <div class="footer-nav">
+            <a href="/home" class="tab active" data-target="home"><i class="fa-solid fa-house"></i></a>
+            <a href="/post" class="tab active" data-target="post"><i class="fas fa-paper-plane"></i></a>
+            <a href="/dmlist" class="tab active" data-target="talk"><i class="fa-solid fa-comment"></i></a>
+            <a href="/profile" class="tab active" data-target="mypage"><i class="fa-solid fa-user"></i></a>
+            <a href="/circle" class="tab active" data-target="circle"><i class="fa-solid fa-cube"></i></i></a>
+          </div>
     </div>
   <script src="{{ asset('js/dm-list.js') }}"></script>
 </body>
