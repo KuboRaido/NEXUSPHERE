fa59e29b9f90d14c425d39ff286d98e119033a78
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2025-12-23 15:04:57 +0900

    複数人とサークル内でdm出来るようにしている途中

--- nexusphere/database/migrations/2025_06_26_074449_create_groups_table.php
+++ nexusphere/database/migrations/2025_06_26_074449_create_groups_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id('group_id');
             $table->string('group_name')->unique();
+            $table->foreignId('circle_id')->nullable()->constrained('circles','circle_id')->cascadeOnDelete();
             $table->timestamps();
         });
     }
