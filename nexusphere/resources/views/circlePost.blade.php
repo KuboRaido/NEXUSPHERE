32a14035f16af07a7e40cdb595e7b7822be0d552
kotomi <SBA24J90009@edu.sba.ac.jp>
2026-01-23 23:44:58 +0900

    notionの8割修正

--- nexusphere/resources/views/circlePost.blade.php
+++ nexusphere/resources/views/circlePost.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>新規投稿 - Nexusphere</title>
+    <title>サークル投稿</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/circlepost.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 <body>
 
