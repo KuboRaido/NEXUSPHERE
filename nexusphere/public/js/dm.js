adcec9cc641b86ea332e29c2b823bb60545626ae
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2026-01-24 11:22:19 +0900

    投稿画面のアイコンを修整した

--- nexusphere/public/js/dm.js
+++ nexusphere/public/js/dm.js
@@ -400,8 +400,6 @@ async function sendMessage() {
   } catch (err) {
     alert(err.message || '送信に失敗しました');
   }
-
-  
 }
 
 //起動
@@ -508,7 +506,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       }
     });
   }
-   const menuBtn = document.getElementById("menu-btn");
+  const menuBtn = document.getElementById("menu-btn");
   const dropdown = document.getElementById("menu-dropdown");
 
     menuBtn.addEventListener("click", () => {
@@ -523,27 +521,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 
     // ユーザー追加
-    document.getElementById("add-user-btn").addEventListener("click", () => {
-        alert("ユーザー追加処理を書く");
-        // 例: モーダル表示 or 別画面遷移
-    });
+    // document.getElementById("add-user-btn").addEventListener("click", () => {
+    //     alert("ユーザー追加処理を書く");
+    //     // 例: モーダル表示 or 別画面遷移
+    // });
 
-    // 退会
-    document.getElementById("leave-chat-btn").addEventListener("click", () => {
-        if (!confirm("このチャットから退会しますか？")) return;
+    // // 退会
+    // document.getElementById("leave-chat-btn").addEventListener("click", () => {
+    //     if (!confirm("このチャットから退会しますか？")) return;
 
-        fetch("/dm/leave", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content
-            },
-            body: JSON.stringify({
-                partner_id: document.getElementById("recipientId").value
-            })
-        }).then(() => {
-            location.href = "/dmlist";
-        });
-    });
+    //     fetch("/dm/leave", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content
+    //         },
+    //         body: JSON.stringify({
+    //             partner_id: document.getElementById("recipientId").value
+    //         })
+    //     }).then(() => {
+    //         location.href = "/dmlist";
+    //     });
+    // });
 
 });
