30f07ed3393bbbc6588288617ecfae72a691c7c6
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2026-01-24 15:25:26 +0900

    メールが贈られるようになった

--- nexusphere/app/Http/Controllers/UserController.php
+++ nexusphere/app/Http/Controllers/UserController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Jobs\SendVerificationEmail;
 use App\Http\Controllers\Controller;
+use App\Mail\VerificationEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
@@ -46,7 +48,8 @@ class UserController extends Controller
             'icon' => $iconPath,
         ]);
 
-        SendVerificationEmail::dispatch($user);
+        // SendVerificationEmail::dispatch($user);
+        Mail::to($user->mail)->send(new VerificationEmail($user));
 
         return redirect()->route('login')->with('success', '登録が完了しました！ログインしてください');
     }
