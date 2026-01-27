4bb65dff44fb180723d3d285f481f69d25173edc
kotomi <SBA24J90009@edu.sba.ac.jp>
2026-01-27 14:33:50 +0900

    コントローラー直しました

--- nexusphere/app/Http/Controllers/UserController.php
+++ nexusphere/app/Http/Controllers/UserController.php
@@ -5,62 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Jobs\SendVerificationEmail;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
-
-class UserController extends Controller
-{
-    public function register(Request $request)
-    {
-        $request->validate([
-            'mail' => ['required','email','unique:users,mail','regex:/@(edu.sba|edu.ssm|sba|ssm)\.ac\.jp$/'],
-            'password' => ['required','string','min:8','max:20','confirmed'],
-            'name' => ['required','string','max:255'],
-            'age' => ['required','integer','min:0','max:120'],
-            'grade' => ['required','integer','min:1','max:4'],
-            'subject' => ['required','string','max:255'],
-            'major' => ['required','string','max:255'],
-            'icon' => ['nullable','image','mimes:jpeg,png,jpg,gif','max:2048'],
-        ]);
-
-        $iconName = null;
-
-        if ($request->hasFile('icon')) {
-            $dir = public_path('icons');
-            if (!is_dir($dir)) {
-                mkdir($dir, 0755, true);
-            }
-
-            $iconName = Str::random(40).'.'.$request->file('icon')->getClientOriginalExtension();
-            $request->file('icon')->move($dir, $iconName);
-        }
-
-        $user = User::create([
-            'mail' => $request->mail,
-            'password' => Hash::make($request->password),
-            'name' => $request->name,
-            'age' => $request->age,
-            'grade' => $request->grade,
-            'subject' => $request->subject,
-            'major' => $request->major,
-            'icon' => $iconName,
-        ]);
-
-        SendVerificationEmail::dispatch($user);
-
-        return redirect()
-            ->route('login')
-            ->with('success', '登録が完了しました！ログインしてください');
-    }
-}
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\User;
-use App\Jobs\SendVerificationEmail;
-use App\Http\Controllers\Controller;
 use App\Mail\VerificationEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
