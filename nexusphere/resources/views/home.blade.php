39a0481647c9c7eb2b1f7f5a2c1d7705149610d4
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2026-01-28 15:41:07 +0900

    リファラは関係無かったため元に戻した

--- nexusphere/resources/views/home.blade.php
+++ nexusphere/resources/views/home.blade.php
@@ -8,7 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <meta name="referrer" content="no-referrer">
 </head>
 <body>
 
