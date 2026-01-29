a153b624e3a7d8d0539ae4396e35af54f8df6adf
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2026-01-29 19:20:43 +0900

    profile編集画面の修正

--- nexusphere/resources/views/profile_edit.blade.php
+++ nexusphere/resources/views/profile_edit.blade.php
@@ -44,7 +44,7 @@
       </div>
 
       <div class="field">
-        <label for="subject">学科</label>
+        <label for="subject" class="label-text">学科</label>
         <select id="subject" name="subject" placeholder="学科を入力" class="input-field" required>
             <option value="" disabled selected>学科を選択してください</option>
             <option value="AI&テクノロジー科">AI&テクノロジー科</option>
@@ -54,7 +54,7 @@
       </div>
 
       <div class="field">
-            <label for="major">専攻</label>
+            <label for="major" class="label-text">専攻</label>
             <select id="major" name="major" class="input-field" required>
                 <option value="" disabled selected>先に学科を選択してください</option>
             </select>
