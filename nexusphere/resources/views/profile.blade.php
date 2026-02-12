d81b3c8d55090097a4bc5cf1b89afe33980f4123
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2026-02-12 11:58:46 +0900

    csrfが無かったため追加

--- nexusphere/resources/views/profile.blade.php
+++ nexusphere/resources/views/profile.blade.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
   <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
