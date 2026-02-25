ea7bb9e32818e64ad96b920bc8acacd6dbc64488
KuboRaido <SBA24J20001@edu.sba.ac.jp>
2026-02-25 15:23:07 +0900

    fix: コンフリクトで消してしまったコードを復元＋httpでpostするようにしてしまっていたからhttpsに修正

--- nexusphere/public/js/profile.js
+++ nexusphere/public/js/profile.js
@@ -43,3 +43,18 @@ document.addEventListener("DOMContentLoaded", function() {
         : '<i class="fa-solid fa-chevron-down"></i>';
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const menuBtn = document.getElementById("menuBtn");
+    const overlay = document.getElementById("overlay");
+
+    menuBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+    overlay.addEventListener("click", function () {
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
